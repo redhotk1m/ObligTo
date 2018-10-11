3a1809c9f84d033b9eb2bcd9fbd2ba44aa86c982
Martina <martinarebekka@gmail.com>
2018-10-11 16:11:17 +0200

    Kommentert oppgave 3 og 4

--- src/Oblig2Test.java
+++ src/Oblig2Test.java
@@ -24,10 +24,10 @@ public class Oblig2Test {
 
         antallFeil += oppgave1();
         antallFeil += oppgave2();
-        //antallFeil += oppgave3();
+        antallFeil += oppgave3();
         antallFeil += oppgave4();
         antallFeil += oppgave5();
-        //antallFeil += oppgave6();
+        antallFeil += oppgave6();
         antallFeil += oppgave7();
         //antallFeil += oppgave8();
         antallFeil += oppgave9();
